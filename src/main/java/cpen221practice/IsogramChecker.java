f2488570c0e6f0d8d21313b960ae8cd9a6678fb9
cbella06 <cbella0627@gmail.com>
2025-10-18 20:52:35 -0700

    finished practice8

--- src/main/java/cpen221practice/IsogramChecker.java
+++ src/main/java/cpen221practice/IsogramChecker.java
@@ -1,9 +1,21 @@
 package cpen221practice;
-
+import java.util.Set;
+import java.util.HashSet;
 class IsogramChecker {
 
     boolean isIsogram(String phrase) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        Set<Character> set= new HashSet<>();
+        String lcase=new String(phrase.toLowerCase());
+        for (int i=0; i<lcase.length();i++){
+            if (lcase.charAt(i)==' ' || lcase.charAt(i)=='-'){
+                continue;
+            }
+            else if(set.contains(lcase.charAt(i))){
+                return false;
+            }
+            set.add(lcase.charAt(i));
+        }
+        return true;
     }
 
 }
